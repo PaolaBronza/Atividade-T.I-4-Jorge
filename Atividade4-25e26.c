6ea0e8ae1ddb4b4620e066e5d5c07ad362de2fc0
PaolaBronza <125701556+PaolaBronza@users.noreply.github.com>
2023-03-16 18:57:02 -0300

    Rename Atividade4-25e26.cpp to Atividade4-25e26.c

--- Atividade4-25e26.c
+++ Atividade4-25e26.c
@@ -7,14 +7,14 @@ int main(void){
 	setlocale(LC_ALL,"portuguese");
 	int num;
 	
-	printf("Insira um número de 1 a 5: ");
+	printf("Insira um nÃºmero de 1 a 5: ");
 	scanf("%d",&num);
 	
 	if(num>=1&&num<=5){
-		printf("Número inserido: %d",num);
+		printf("NÃºmero inserido: %d",num);
 	}
 	else if(num<0&&num>=6){
-		printf("Número incorreto");
+		printf("NÃºmero incorreto");
 	}
 	return 0;
 }
