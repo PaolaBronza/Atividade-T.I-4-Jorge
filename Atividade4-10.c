6393917cc096f8370b3900d30a8175ea7a1fbb52
PaolaBronza <125701556+PaolaBronza@users.noreply.github.com>
2023-03-16 18:54:29 -0300

    Rename Atividade4-10.cpp to Atividade4-10.c

--- Atividade4-10.c
+++ Atividade4-10.c
@@ -7,17 +7,17 @@ int main(void){
 	setlocale(LC_ALL,"portuguese");
 	int num;
 	
-	printf("Digite um número: ");
+	printf("Digite um nÃºmero: ");
 	scanf("%d",&num);
 	
 	if(num>100&&num<200){
-		printf("%d está entre 100 e 200",num);
+		printf("%d estÃ¡ entre 100 e 200",num);
 	}
 	else if(num<100){
-		printf("%d é menor que 100",num);
+		printf("%d Ã© menor que 100",num);
 	}
 	else if(num>200){
-			printf("%d é maior que 200",num);
+			printf("%d Ã© maior que 200",num);
 		}
 	return 0;
 }
